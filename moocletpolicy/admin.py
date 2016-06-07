5682562479fd96336ce28f5b4b9dc8f804ae4737
Sam Maldonado <sam.maldonado@cognitivescience.co>
2016-06-06 22:56:53 -0700

    Callable get_mooclet for admin

--- moocletpolicy/admin.py
+++ moocletpolicy/admin.py
@@ -21,10 +21,13 @@ class SubGroupProbabilityArrayAdmin (admin.ModelAdmin):
 	list_display = ['id', 'mooclet', 'subgroup']
 
 class VersionProbabilityAdmin (admin.ModelAdmin):
-	list_display = ['id', 'version__mooclet', 'version', 'subgroup_probability_array', 'probability']
+	list_display = ['id', 'get_mooclet', 'version', 'subgroup_probability_array', 'probability']
 
 	#__ signifies looking up property of mooclet ForeignKey
-	list_filter = ('version__mooclet', 'version', 'subgroup_probability_array')
+	list_filter = ('get_mooclet', 'version', 'subgroup_probability_array')
+
+	def get_mooclet(self, obj):
+		return obj.version.mooclet
 
 	
 
