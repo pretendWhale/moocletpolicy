f63ac9cb6e75d1677f0b1b7ac8297b40886b4221
Sam Maldonado <sam.maldonado@cognitivescience.co>
2016-06-06 21:41:05 -0700

    Initial admin views

--- moocletpolicy/admin.py
+++ moocletpolicy/admin.py
@@ -6,5 +6,24 @@ from .models import *
 
 app = apps.get_app_config('moocletpolicy')
 
-for model_name, model in app.models.items():
-    admin.site.register(model)
+class MoocletAdmin (admin.ModelAdmin):
+	list_display = ['id','name']
+
+class VersionAdmin (admin.ModelAdmin):
+	list_display = ['id', 'mooclet', 'name']
+
+class SubGroupAdmin (admin.ModelAdmin):
+	list_display = ['id', 'var1', 'var2', 'var3', 'var4', 'var5', 'var6', 'var7']
+
+class SubGroupProbabilityArrayAdmin (admin.ModelAdmin):
+	list_display = ['id', 'mooclet', 'subgroup']
+
+class VersionProbabilityArrayAdmin (admin.ModelAdmin):
+	list_display = ['id', 'version', 'subgroup_probability_array', 'probability']
+
+
+admin.site.register(Mooclet, MoocletAdmin)
+admin.site.register(Version, VersionAdmin)
+admin.site.register(SubGroup, SubGroupAdmin)
+admin.site.register(SubGroupProbabilityArray, SubGroupProbabilityArrayAdmin)
+admin.site.register(VersionProbabilityArray, VersionProbabilityArrayAdmin)
