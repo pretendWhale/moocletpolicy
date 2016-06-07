8933ea4e0fa6d3df30a0084ff148b7bc0697b459
Sam Maldonado <sam.maldonado@cognitivescience.co>
2016-06-06 21:58:12 -0700

    added basic admin filtering

--- moocletpolicy/admin.py
+++ moocletpolicy/admin.py
@@ -11,6 +11,7 @@ class MoocletAdmin (admin.ModelAdmin):
 
 class VersionAdmin (admin.ModelAdmin):
 	list_display = ['id', 'mooclet', 'name']
+	list_filter = ('mooclet')
 
 class SubGroupAdmin (admin.ModelAdmin):
 	list_display = ['id', 'var1', 'var2', 'var3', 'var4', 'var5', 'var6', 'var7']
@@ -20,6 +21,7 @@ class SubGroupProbabilityArrayAdmin (admin.ModelAdmin):
 
 class VersionProbabilityAdmin (admin.ModelAdmin):
 	list_display = ['id', 'version', 'subgroup_probability_array', 'probability']
+	list_filter = ('version', 'subgroup_probability_array')
 
 
 admin.site.register(Mooclet, MoocletAdmin)
