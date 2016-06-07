3c192b07a53a6762d7de4b5b05670d4ca55a6037
Sam Maldonado <sam.maldonado@cognitivescience.co>
2016-06-06 22:08:56 -0700

    Filter with foreignkey names/ids

--- moocletpolicy/admin.py
+++ moocletpolicy/admin.py
@@ -10,8 +10,11 @@ class MoocletAdmin (admin.ModelAdmin):
 	list_display = ['id','name']
 
 class VersionAdmin (admin.ModelAdmin):
-	list_display = ['id', 'mooclet', 'name']
-	list_filter = ('mooclet')
+	list_display = ['id', 'get_mooclet_id', 'name']
+	list_filter = ('get_mooclet_id')
+
+	def get_mooclet_id(self,obj):
+		return obj.mooclet.id
 
 class SubGroupAdmin (admin.ModelAdmin):
 	list_display = ['id', 'var1', 'var2', 'var3', 'var4', 'var5', 'var6', 'var7']
@@ -20,8 +23,17 @@ class SubGroupProbabilityArrayAdmin (admin.ModelAdmin):
 	list_display = ['id', 'mooclet', 'subgroup']
 
 class VersionProbabilityAdmin (admin.ModelAdmin):
-	list_display = ['id', 'version', 'subgroup_probability_array', 'probability']
-	list_filter = ('version', 'subgroup_probability_array')
+	list_display = ['id', 'get_mooclet_id', 'get_version_name', 'get_subgroup_probability_array_id', 'probability']
+	list_filter = ('get_mooclet_id', 'get_version_name', 'get_subgroup_probability_array_id')
+
+	def get_version_name(self,obj):
+		return obj.version.name
+
+	def get_mooclet_id(self,obj):
+		return obj.version.mooclet.id
+
+	def get_subgroup_probability_array_id(self,obj):
+		return obj.subgroup_probability_array.id
 
 
 admin.site.register(Mooclet, MoocletAdmin)
