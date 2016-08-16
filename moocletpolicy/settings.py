2e034675dd0774f341a15e1526665fd556b0c742
Samuel Maldonado <sam.maldonado@cognitivescience.co>
2016-08-15 18:41:48 -0700

    In progress preparation to deploy

--- moocletpolicy/settings.py
+++ moocletpolicy/settings.py
@@ -93,6 +93,13 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db_reorg.sqlite3'),
+#         }
+# }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
